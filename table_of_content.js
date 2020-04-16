7538cb89223097ee6d49cbf0c79e2d8bd33ee785
Tetiana Klymchuk <missklimchuck@yandex.ua>
2020-04-16 09:35:25 +0200

    [NEW] Added table of content js

---
+++ table_of_content.js
@@ -0,0 +1,45 @@
+function createTOC(){
+    var toc = "";
+    var level = 0;
+    var levels = {}
+    $('#toc').html('');
+
+    $(":header").each(function(i){
+        if (this.id =='tocheading' || this.hasAttribute('data-skip-toc')) return;
+        
+	    var titleText = this.innerHTML;
+	    var openLevel = this.tagName[1];
+
+	    if (levels[openLevel]){
+		    levels[openLevel] += 1;
+	    } else{
+		    levels[openLevel] = 1;
+	    }
+
+	    if (openLevel > level) {
+		    toc += (new Array(openLevel - level + 1)).join('<ul class="toc">');
+	    } else if (openLevel < level) {
+		    toc += (new Array(level - openLevel + 1)).join("</ul>");
+		    for (i=level;i>openLevel;i--){levels[i]=0;}
+	    }
+
+	    level = parseInt(openLevel);
+
+
+	    if (this.id==''){this.id = this.innerHTML.replace(/ /g,"-")}
+	    var anchor = this.id;
+        
+	    toc += '<li><a href="#' + encodeURIComponent(anchor) + '">'
+		+ romanize(levels[openLevel].toString()) + '. ' + titleText
+		+ '</a></li>';
+        
+	});
+
+    
+    if (level) {
+	    toc += (new Array(level + 1)).join("</ul>");
+    }
+
+ 
+    $('#toc').append(toc);
+};
